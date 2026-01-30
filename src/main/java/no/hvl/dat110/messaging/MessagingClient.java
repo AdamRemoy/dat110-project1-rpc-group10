3616be137b194ebee573bfac278235d5a64d148e
Adam <adam.remoy@gmail.com>
2026-01-30 13:00:45 +0100

    implemented the methods for the client-side of the messaging service

--- src/main/java/no/hvl/dat110/messaging/MessagingClient.java
+++ src/main/java/no/hvl/dat110/messaging/MessagingClient.java
@@ -1,7 +1,9 @@
 package no.hvl.dat110.messaging;
 
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import no.hvl.dat110.TODO;
 
@@ -25,15 +27,16 @@ public class MessagingClient {
 		Socket clientSocket;
 
 		MessageConnection connection = null;
-		
-		// TODO - START
-		// connect to messaging server using a TCP socket
-		// create and return a corresponding messaging connection
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
+
+    try {
+      clientSocket = new Socket(server, port);
+      connection = new MessageConnection(clientSocket);
+    } catch (UnknownHostException unknownHostException) {
+      System.out.println("Unknown host: " + unknownHostException.toString());
+    } catch (IOException ioException) {
+      System.out.println("Unknown input: " + ioException.toString());
+    }
+
 		return connection;
 	}
 }
