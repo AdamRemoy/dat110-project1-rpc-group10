5294b29cee5cc61d9b7f3e05a1dd6e3025327568
Adam <adam.remoy@gmail.com>
2026-01-30 15:15:20 +0100

    implemented encap, decap and marshall methods

--- src/main/java/no/hvl/dat110/rpc/RPCUtils.java
+++ src/main/java/no/hvl/dat110/rpc/RPCUtils.java
@@ -8,141 +8,64 @@ public class RPCUtils {
 	
 	public static byte[] encapsulate(byte rpcid, byte[] payload) {
 		
-		byte[] rpcmsg = null;
-		
-		// TODO - START
-		
-		// Encapsulate the rpcid and payload in a byte array according to the RPC message syntax / format
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
+		byte[] rpcmsg = new byte[payload.length + 1];
+
+    rpcmsg[0] = rpcid;
+
+    for (int i = 0; i < payload.length; i++) {
+      rpcmsg[i + 1] = payload[i];
+    }
+
 		return rpcmsg;
 	}
 	
 	public static byte[] decapsulate(byte[] rpcmsg) {
-		
-		byte[] payload = null;
-		
-		// TODO - START
-		
-		// Decapsulate the rpcid and payload in a byte array according to the RPC message syntax
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return payload;
-		
+		return Arrays.copyOfRange(rpcmsg, 1, rpcmsg.length);
 	}
 
 	// convert String to byte array
 	public static byte[] marshallString(String str) {
-		
-		byte[] encoded = null;
-		
-		// TODO - START 
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return encoded;
+		return str.getBytes();
 	}
 
 	// convert byte array to a String
 	public static String unmarshallString(byte[] data) {
-		
-		String decoded = null; 
-		
-		// TODO - START 
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return decoded;
+		return new String(data);
 	}
 	
 	public static byte[] marshallVoid() {
-		
-		byte[] encoded = null;
-		
-		// TODO - START 
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-				
-		// TODO - END
-		
-		return encoded;
-		
+    return new byte[0];
 	}
 	
-	public static void unmarshallVoid(byte[] data) {
-		
-		// TODO
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-	}
+	public static void unmarshallVoid(byte[] data) { }
 
 	// convert boolean to a byte array representation
 	public static byte[] marshallBoolean(boolean b) {
 		
 		byte[] encoded = new byte[1];
-				
-		if (b) {
-			encoded[0] = 1;
-		} else
-		{
-			encoded[0] = 0;
-		}
-		
+    encoded[0] = (b) ? (byte) 1 : (byte) 0;
+
 		return encoded;
 	}
 
 	// convert byte array to a boolean representation
 	public static boolean unmarshallBoolean(byte[] data) {
-		
 		return (data[0] > 0);
-		
 	}
 
 	// integer to byte array representation
 	public static byte[] marshallInteger(int x) {
+
+    ByteBuffer buffer = ByteBuffer.allocate(4);
+    buffer.putInt(x);
 		
-		byte[] encoded = null;
-		
-		// TODO - START 
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return encoded;
+		return buffer.array();
 	}
 	
 	// byte array representation to integer
 	public static int unmarshallInteger(byte[] data) {
-		
-		int decoded = 0;
-		
-		// TODO - START 
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
-		
-		return decoded;
+    ByteBuffer buffer = ByteBuffer.wrap(data);
+		return buffer.getInt();
 		
 	}
 }
