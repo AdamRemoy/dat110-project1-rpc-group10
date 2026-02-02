e11b3abc5d2fe1477f7bc0546ac05fef6e8f65bc
Adam <adam.remoy@gmail.com>
2026-02-02 14:31:29 +0100

    implemented the client-side stub

--- src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
+++ src/main/java/no/hvl/dat110/system/controller/DisplayStub.java
@@ -10,15 +10,11 @@ public class DisplayStub extends RPCLocalStub {
 	}
 	
 	public void write (String message) {
-		
-		// TODO - START
-		
-		// implement marshalling, call and unmarshalling for write RPC method
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - END
+
+    byte[] req = RPCUtils.marshallString(message);
+    byte[] res = rpcclient.call((byte)Common.WRITE_RPCID, req);
+
+    RPCUtils.unmarshallVoid(res);
 		
 	}
 }
