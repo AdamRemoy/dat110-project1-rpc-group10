1da510b67d46b9956bf57bd6b713cff2e7f80825
Adam <adam.remoy@gmail.com>
2026-02-10 14:10:46 +0100

    finished senor implementation

--- src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
+++ src/main/java/no/hvl/dat110/system/display/DisplayDevice.java
@@ -10,13 +10,9 @@ public class DisplayDevice {
 	public static void main(String[] args) {
 		
 		System.out.println("Display server starting ...");
-		
-		// TODO - START
-		// implement the operation of the display RPC server
-		// see how this is done for the sensor RPC server in SensorDevice
 
     RPCServer displayServer = new RPCServer(Common.DISPLAYPORT);
-    DisplayImpl display = new DisplayImpl((byte) Common.READ_RPCID, displayServer);
+    DisplayImpl display = new DisplayImpl((byte) Common.WRITE_RPCID, displayServer);
 
     displayServer.run();
     displayServer.stop();
